39d23d32918c938c216c8a932bf1d1174fe03e59
knowak013 <kamila.nowak013@gmail.com>
2023-04-22 23:19:26 +0200

    add search function and refactor renderBox

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ constructor(){
 initializeApp = () => {
     this.connectDomElements();
     this.setupListenners();
-    this.renderBox();
+    this.renderAllBox();
 
    // this.getData().then((dataSet) => {console.log(dataSet[1].category)});
 }
@@ -19,88 +19,95 @@ connectDomElements = () =>{
     this.form = document.querySelector("form");
     this.input = document.getElementById("searchInput");
     this.recomendedBox = document.getElementById("recomendedBox");
+    this.h1 =document.getElementById("h1")
 
 }
 
 setupListenners = () =>{
    this.form.addEventListener("submit", e => {
         e.preventDefault();
-
-        console.log(this.input.value);
+        this.h1.innerText=""
+        this.searchByWord(this.input.value);
     })
 }
 
+searchByWord(word){
+    word = word.toLowerCase();
+    console.log(this.input.value);
+    this.recomendedBox.innerHTML = "";
+    let i=0;
+    for(let data of this.dataSets){
+        const dataTitle = data.title.toLowerCase();
+     
+        if(dataTitle.includes(word))
+        {
+            i++;
+            this.recomendedBox.appendChild(this.createBox(data));
+         
+        }
 
+        if(i==0){
+            this.h1.innerText="No results found for '"+word+"'"
+        }else if(i==1){
+            this.h1.innerText="Found "+i+" result for '"+word+"'"
+        }else{
+            this.h1.innerText="Found "+i+" results for '"+word+"'"
+        }
+    }
+}
 
-getData = () => fetch('assets/data.json').then(response => response.json());
-
-
-
-renderBox = async () =>{
+renderAllBox = async () =>{
     this.recomendedBox.innerHTML = "";
 
-    const dataSets = await this.getData();
+    this.dataSets = await this.getData();
 
-    for(let data of dataSets){
-     console.log(data);
-
- 
-
-        const  recomendedBoxItem = createDomElement('div',"recomended--box--item");
-
-        const recomendedBoxItemBookmark = createDomElement('div',"recomended--box--item-bookmark",null,null); //icona bookmark
-        const recomendedBoxItemBookmarkIcon = createDomElement('img',"recomended--box--item-bookmark-icon",null,"assets/icon-bookmark-empty.svg")//icona bookmark
-        const recomendedBoxItemImg = createDomElement("img","recomended--box--item-img",null,data.thumbnail.regular.small)
-        const recomendedBoxItemDesc =createDomElement("div","recomended--box--item-desc")
-        const year = createDomElement("p",null,data.year,null)
-        const recomendedBoxItemDescDot = createDomElement("div","recomended--box--item-desc-dot")
-        const recomendedBoxItemDescDot2 = createDomElement("div","recomended--box--item-desc-dot")
-        const recomendedBoxItemDescCategory =createDomElement("div","recomended--box--item-desc-category")
-        let imgCategory;
-        if(data.category == "Movie"){
-            imgCategory = createDomElement("img","img-category",null,"assets/icon-category-movie.svg")
-        }else{
-            imgCategory = createDomElement("img","img-category",null,"assets/icon-category-tv.svg")
-        }
-        const textCategory =createDomElement("p",null,data.category,null)
-        const recomendedBoxItemDescCategoryRating = createDomElement("p",null,data.rating)
-        const h2 = createDomElement("h2",null,data.title)
-
-       
-
-
-
-
-        recomendedBoxItemBookmark.appendChild(recomendedBoxItemBookmarkIcon);//div icona bookmark
-        recomendedBoxItemDescCategory.appendChild(imgCategory) //div z kategoria
-        recomendedBoxItemDescCategory.appendChild(textCategory) //div z kategoria
-
-        recomendedBoxItemDesc.appendChild(year); // div z opisem
-        recomendedBoxItemDesc.appendChild(recomendedBoxItemDescDot);// div z opisem
-        recomendedBoxItemDesc.appendChild(recomendedBoxItemDescCategory);// div z opisem
-        recomendedBoxItemDesc.appendChild(recomendedBoxItemDescDot2);// div z opisem
-        recomendedBoxItemDesc.appendChild(recomendedBoxItemDescCategoryRating);// div z opisem
-
-
-        recomendedBoxItem.appendChild( recomendedBoxItemBookmark)///icona bookmark
-        recomendedBoxItem.appendChild(recomendedBoxItemImg)// image
-        recomendedBoxItem.appendChild(recomendedBoxItemDesc)//opis
-        recomendedBoxItem.appendChild(h2) //tytul
-
-
-
-        
-       /* const h2 = createDomElement('h2',null,data.title,null)
-
-        recomendedBoxItem.appendChild(h2);*/
-
-        this.recomendedBox.appendChild(recomendedBoxItem);
+    for(let data of this.dataSets){
+        this.recomendedBox.appendChild(this.createBox(data));
     }
 }
 
 
+getData = () => fetch('assets/data.json').then(response => response.json());
+
+createBox = (data) => {
+
+    const recomendedBoxItem = createDomElement('div',"recomended--box--item");
+    const recomendedBoxItemBookmark = createDomElement('div',"recomended--box--item-bookmark",null,null); //icona bookmark
+    const recomendedBoxItemBookmarkIcon = createDomElement('img',"recomended--box--item-bookmark-icon",null,"assets/icon-bookmark-empty.svg");//icona bookmark
+    const recomendedBoxItemImg = createDomElement("img","recomended--box--item-img",null,data.thumbnail.regular.small);
+    const recomendedBoxItemDesc =createDomElement("div","recomended--box--item-desc");
+    const year = createDomElement("p",null,data.year,null);
+    const recomendedBoxItemDescDot = createDomElement("div","recomended--box--item-desc-dot");
+    const recomendedBoxItemDescDot2 = createDomElement("div","recomended--box--item-desc-dot");
+    const recomendedBoxItemDescCategory =createDomElement("div","recomended--box--item-desc-category");
+    let imgCategory;
+    if(data.category == "Movie"){
+        imgCategory = createDomElement("img","img-category",null,"assets/icon-category-movie.svg");
+    }else{
+        imgCategory = createDomElement("img","img-category",null,"assets/icon-category-tv.svg");
+    }
+    const textCategory =createDomElement("p",null,data.category,null);
+    const recomendedBoxItemDescCategoryRating = createDomElement("p",null,data.rating);
+    const h2 = createDomElement("h2",null,data.title);
+
+    recomendedBoxItemBookmark.appendChild(recomendedBoxItemBookmarkIcon);//div icona bookmark
+    recomendedBoxItemDescCategory.appendChild(imgCategory); //div z kategoria
+    recomendedBoxItemDescCategory.appendChild(textCategory); //div z kategoria
+
+    recomendedBoxItemDesc.appendChild(year); // div z opisem
+    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescDot);// div z opisem
+    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescCategory);// div z opisem
+    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescDot2);// div z opisem
+    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescCategoryRating);// div z opisem
+
+
+    recomendedBoxItem.appendChild( recomendedBoxItemBookmark);///icona bookmark
+    recomendedBoxItem.appendChild(recomendedBoxItemImg);// image
+    recomendedBoxItem.appendChild(recomendedBoxItemDesc);//opis
+    recomendedBoxItem.appendChild(h2) //tytul
+    
+    return recomendedBoxItem;
 }
-
-
+}
 
 document.addEventListener("DOMContentLoaded", new EnterteimentWebApp());
