2aea339fccbdde170be10b1ae557cc4bc68ea9ef
knowak013 <kamila.nowak013@gmail.com>
2023-05-03 20:11:42 +0200

    Added fav, tablet and desktop version, search by letter

--- js/app.js
+++ js/app.js
@@ -10,31 +10,117 @@ constructor(){
 initializeApp = () => {
     this.connectDomElements();
     this.setupListenners();
-    this.renderAllBox();
-
-   // this.getData().then((dataSet) => {console.log(dataSet[1].category)});
+    this.renderAllBoxes();
+    this.navItemsHome.classList.add("active");
 }
 
 connectDomElements = () =>{
     this.form = document.querySelector("form");
     this.input = document.getElementById("searchInput");
-    this.recomendedBox = document.getElementById("recomendedBox");
-    this.h1 =document.getElementById("h1")
-
+    this.box = document.getElementById("box");
+    this.h1 = document.getElementById("h1");
+    this.navItemsHome = document.getElementById("navItemsHome");
+    this.navItemsMovies = document.getElementById("navItemsMovies");
+    this.navItemsTVSeries = document.getElementById("navItemsTVSeries");
+    this.navItemsFavorite = document.getElementById("navItemsFavorite");
+    this.navelements = document.querySelectorAll("a");
 }
 
 setupListenners = () =>{
-   this.form.addEventListener("submit", e => {
+
+/*
+    this.form.addEventListener("submit", e => {
         e.preventDefault();
         this.h1.innerText=""
         this.searchByWord(this.input.value);
+        form.reset();
+    })*/
+   this.form.addEventListener("submit", e => {
+        e.preventDefault();
+
     })
+    this.input.addEventListener("input", e => {
+       
+       setTimeout(()=>{
+        this.h1.innerText=""
+        this.searchByWord(this.input.value);
+       },1000)
+
+    })
+
+    this.navItemsHome.addEventListener("click", () => {
+        this.h1.innerText="Recommended for you"
+        this.navelements.forEach(element =>{
+            element.classList.remove("active");
+        })
+        this.navItemsHome.classList.add("active");
+        this.renderAllBoxes();
+    }); 
+
+    this.navItemsMovies.addEventListener("click", () => {
+        this.h1.innerText="Movies"
+        this.navelements.forEach(element =>{
+            element.classList.remove("active");
+        })
+        this.navItemsMovies.classList.add("active");
+        this.searchByCategory("Movie");
+    }); 
+
+    this.navItemsTVSeries.addEventListener("click", () => {
+        this.h1.innerText="TV Series";
+        this.navelements.forEach(element =>{
+            element.classList.remove("active");
+        })
+        this.navItemsTVSeries.classList.add("active");
+        this.searchByCategory("TV Series");
+    }); 
+
+    this.navItemsFavorite.addEventListener("click", () => {
+        this.h1.innerText="Bookmarked Items";
+        this.navelements.forEach(element =>{
+            element.classList.remove("active");
+        })
+        this.navItemsFavorite.classList.add("active");
+        this.showFav();
+    }); 
+
+}   
+
+showFav(){
+    this.box.innerHTML = "";
+    let i=0;
+    for(let data of this.dataSets){
+        
+    if(localStorage.getItem(data.title)==="true")
+    {
+        i++;
+        this.box.appendChild(this.createBox(data));
+    }
+
+}     console.log(i)
+        if(i===0){
+            this.h1.innerText="You don't have any bookmarked items" 
+            console.log(i)
+        }
+}
+
+
+searchByCategory(category){
+    this.box.innerHTML = "";
+    for(let data of this.dataSets){
+        
+     
+        if(data.category.includes(category))
+        {
+            this.box.appendChild(this.createBox(data));
+        }
+    }
+
 }
 
 searchByWord(word){
     word = word.toLowerCase();
-    console.log(this.input.value);
-    this.recomendedBox.innerHTML = "";
+    this.box.innerHTML = "";
     let i=0;
     for(let data of this.dataSets){
         const dataTitle = data.title.toLowerCase();
@@ -42,7 +128,7 @@ searchByWord(word){
         if(dataTitle.includes(word))
         {
             i++;
-            this.recomendedBox.appendChild(this.createBox(data));
+            this.box.appendChild(this.createBox(data));
          
         }
 
@@ -56,57 +142,76 @@ searchByWord(word){
     }
 }
 
-renderAllBox = async () =>{
-    this.recomendedBox.innerHTML = "";
+renderAllBoxes = async () =>{
+    this.box.innerHTML = "";
 
     this.dataSets = await this.getData();
 
     for(let data of this.dataSets){
-        this.recomendedBox.appendChild(this.createBox(data));
+        this.box.appendChild(this.createBox(data));
     }
 }
 
-
 getData = () => fetch('assets/data.json').then(response => response.json());
+
+
 
 createBox = (data) => {
 
-    const recomendedBoxItem = createDomElement('div',"recomended--box--item");
-    const recomendedBoxItemBookmark = createDomElement('div',"recomended--box--item-bookmark",null,null); //icona bookmark
-    const recomendedBoxItemBookmarkIcon = createDomElement('img',"recomended--box--item-bookmark-icon",null,"assets/icon-bookmark-empty.svg");//icona bookmark
-    const recomendedBoxItemImg = createDomElement("img","recomended--box--item-img",null,data.thumbnail.regular.small);
-    const recomendedBoxItemDesc =createDomElement("div","recomended--box--item-desc");
-    const year = createDomElement("p",null,data.year,null);
-    const recomendedBoxItemDescDot = createDomElement("div","recomended--box--item-desc-dot");
-    const recomendedBoxItemDescDot2 = createDomElement("div","recomended--box--item-desc-dot");
-    const recomendedBoxItemDescCategory =createDomElement("div","recomended--box--item-desc-category");
-    let imgCategory;
+    const item = createDomElement('div',"item");
+    const itemBookmark = createDomElement('div',"item-bookmark",null,null); //icona bookmark
+    //let itemBookmarkIcon = createDomElement('img',"item-bookmark-icon",null,"assets/icon-bookmark-empty.svg")  ;//icona bookmark
+    let itemBookmarkIcon = createDomElement('img',"item-bookmark-icon",null,null)  ;//icona bookmark
+     if(localStorage.getItem(data.title)==="true"){
+        itemBookmarkIcon.src = "assets/icon-bookmark-full.svg"
+     }else{
+        itemBookmarkIcon.src = "assets/icon-bookmark-empty.svg"
+     }
+    const itemImg = createDomElement("img","item-img",null,data.thumbnail.regular.small);
+    const itemDesc =createDomElement("div","item-desc");
+    const itemyear = createDomElement("p",null,data.year,null);
+    const itemDescDot = createDomElement("div","item-desc-dot");
+    const itemDescDot2 = createDomElement("div","item-desc-dot");
+    const itemDescCategory =createDomElement("div","item-desc-category");
+    let itemImgCategory;
     if(data.category == "Movie"){
-        imgCategory = createDomElement("img","img-category",null,"assets/icon-category-movie.svg");
+        itemImgCategory = createDomElement("img","img-category",null,"assets/icon-category-movie.svg");
     }else{
-        imgCategory = createDomElement("img","img-category",null,"assets/icon-category-tv.svg");
+        itemImgCategory = createDomElement("img","img-category",null,"assets/icon-category-tv.svg");
     }
     const textCategory =createDomElement("p",null,data.category,null);
-    const recomendedBoxItemDescCategoryRating = createDomElement("p",null,data.rating);
+    const itemDescCategoryRating = createDomElement("p",null,data.rating);
     const h2 = createDomElement("h2",null,data.title);
 
-    recomendedBoxItemBookmark.appendChild(recomendedBoxItemBookmarkIcon);//div icona bookmark
-    recomendedBoxItemDescCategory.appendChild(imgCategory); //div z kategoria
-    recomendedBoxItemDescCategory.appendChild(textCategory); //div z kategoria
+    itemBookmark.appendChild(itemBookmarkIcon);//div icona bookmark
+    itemDescCategory.appendChild(itemImgCategory); //div z kategoria
+    itemDescCategory.appendChild(textCategory); //div z kategoria
 
-    recomendedBoxItemDesc.appendChild(year); // div z opisem
-    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescDot);// div z opisem
-    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescCategory);// div z opisem
-    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescDot2);// div z opisem
-    recomendedBoxItemDesc.appendChild(recomendedBoxItemDescCategoryRating);// div z opisem
+    itemDesc.appendChild(itemyear); // div z opisem
+    itemDesc.appendChild(itemDescDot);// div z opisem
+    itemDesc.appendChild(itemDescCategory);// div z opisem
+    itemDesc.appendChild(itemDescDot2);// div z opisem
+    itemDesc.appendChild(itemDescCategoryRating);// div z opisem
 
 
-    recomendedBoxItem.appendChild( recomendedBoxItemBookmark);///icona bookmark
-    recomendedBoxItem.appendChild(recomendedBoxItemImg);// image
-    recomendedBoxItem.appendChild(recomendedBoxItemDesc);//opis
-    recomendedBoxItem.appendChild(h2) //tytul
-    
-    return recomendedBoxItem;
+    item.appendChild(itemBookmark);///icona bookmark
+    item.appendChild(itemImg);// image
+    item.appendChild(itemDesc);//opis
+    item.appendChild(h2); //tytul
+    let isFavourite;
+
+    itemBookmark.addEventListener("click", () => {
+        if(localStorage.getItem(data.title)==="true"){
+            isFavourite = false;
+            itemBookmarkIcon.src = "assets/icon-bookmark-empty.svg"
+            localStorage.setItem(data.title,isFavourite);
+        }else{
+            isFavourite = true;
+            itemBookmarkIcon.src = "assets/icon-bookmark-full.svg"
+            localStorage.setItem(data.title,isFavourite);
+        } 
+    })
+    return item;
 }
 }
 
